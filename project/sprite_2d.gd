41a7926ba3206d28103f68077d13a551d90f9eb0
victorliu5296 <victorliu5296@outlook.com>
2024-05-31 07:46:57 -0400

    Add basic icon

--- project/sprite_2d.gd
+++
@@ -1,9 +0,0 @@
-extends Sprite2D
-
-var speed: float = 400
-var angular_speed: float = PI
-
-func _process(delta):
-    rotation += angular_speed * delta
-    var velocity = Vector2.UP.rotated(rotation) * speed
-    position += velocity * delta
