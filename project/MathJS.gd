b15e43dcf55e8a468e1034cc1518a2bead0f7cab
victorliu5296 <victorliu5296@outlook.com>
2024-06-22 15:07:47 -0400

    Working KaTeX rendering + mathJS connection to Godot

--- project/MathJS.gd
+++ project/MathJS.gd
@@ -8,6 +8,13 @@ func _ready():
         """)
     else:
         print("The JavaScriptBridge singleton is NOT available")
+        return
     
     var mathJS = JavaScriptBridge.get_interface("math")
     print(mathJS.add(1, 2))
+    print(mathJS.derivative('x^2', 'x').toString())
+    
+    var renderLatex = func(katexExpression: String) -> void:
+        JavaScriptBridge.eval("renderLatex('" + katexExpression.replace("\\", "\\\\") + "')")
+
+    renderLatex.call("\\sqrt{a^2 + b^2}")
